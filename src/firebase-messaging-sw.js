35d8c0bef37c9dcf22d3c054d64c2a3b00f7c147
Julio Luna <julioluna1404@gmail.com>
2021-10-11 00:41:43 -0500

    added news values

--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -1,6 +1,6 @@
 importScripts('https://www.gstatic.com/firebasejs/5.4.1/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/5.4.1/firebase-messaging.js');
 firebase.initializeApp({
-  'messagingSenderId': ‘YOUR_SENDER_ID’
+  'messagingSenderId': "251511397268"
 });
 const messaging = firebase.messaging();
